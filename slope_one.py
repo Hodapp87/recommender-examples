7b22ba78873f50349888e621ebe486950ec32a4f
Chris Hodapp <hodapp87@gmail.com>
2018-02-03 09:23:26 -0500

    Moved Slope One implementation elsewhere
    Also attempted to compute deviation based on the data before its
    conversion to a matrix, but this is much slower than the combined
    process of turning it to a dense matrix, then computing from the
    matrix.

---
+++ slope_one.py
@@ -0,0 +1,151 @@
+###########################################################################
+# slope_one.py: Slope One Predictors implementation for recommender system
+# Author: Chris Hodapp (hodapp87@gmail.com)
+# Date: 2018-02-02
+###########################################################################
+
+"""Module implementing Slope One Predictors over NumPy arrays.
+
+Slope One Predictors come from the paper arXiv:cs/0702144v2, "Slope
+One Predictors for Online Rating-Based Collaborative Filtering,"
+by Daniel Lemire and Anna Maclachlan.
+"""
+
+import numpy as np
+
+def deviation_from_df(data):
+    """Computes the average deviation described in the "Slope One
+    Predictors" paper, and returns this in matrix form, along with a
+    matrix giving the number of users who have rated each pair of
+    items.
+
+    For the returned 'dev' matrix, element [j,i] gives the average
+    deviation of item i with respect to item j.  Note that dev[i,j] =
+    -dev[j,i].
+    For 'counts', element [i,j] gives the number of users who have
+    rated both item i and item j.
+
+    Parameters:
+    data -- Pandas dataframe; needs columns "user_id", "movie_id", "rating"
+
+    Returns:
+    dev -- NumPy array of shape (n,n), if M.shape = (m,n).
+    counts -- NumPy array of shape (n,n)
+    """
+    n = data["movie_id"].max() + 1
+    m = user["user_id"].max() + 1
+    movie2idx = [np.where(user["movie_id"] == i)[0] for i in range(n)]
+    user2idx = [np.where(user["user_id"] == i)[0] for i in range(m)]    
+    diff = np.zeros((n,n))
+    dev = np.zeros((n,n))
+    counts = np.zeros((n,n))    
+    for i in range(n):
+        # 'i' = the index of one movie
+        for row in user.iloc[movie2idx[i]].itertuples():
+            # u = user who rated movie i
+            u = row.user_id
+            # u_i = user u's rating on movie i
+            u_i = row.rating
+            # row.movie_id = i (due to how movie2idx is made)
+            others = user.iloc[user2idx[u]]
+            # others = dataframe of ratings, all of which are by user
+            # u.  Then u_js is a series of ratings for those movies,
+            # and js is the movie indices corresponding to those:
+            u_js, js = others["rating"], others["movie_id"]
+            # As user u also rated movie i, all other ratings here are
+            # (combined with u_i): a pair of ratings by the same user.
+            # We can exploit some symmetry too:
+            mask = js > i
+            u_js, js = u_js[mask], js[mask]
+            # So, we may add up u_j - u_i below (and for the flipped
+            # indices it's simply negated):
+            d = u_js - u_i
+            diff[i, js] -= d
+            diff[js, i] += d
+            # and count the ratings, so we may compute an average:
+            counts[i, js] += 1
+            counts[js, i] += 1
+    # This is still much slower than the version that starts from a
+    # matrix.
+    dev = diff / np.maximum(1, counts)
+    return dev, counts
+
+def deviation(M, mask):
+    """Computes the average deviation described in the "Slope One
+    Predictors" paper, and returns this in matrix form, along with a
+    matrix giving the number of users who have rated each pair of
+    items.
+
+    For the returned 'dev' matrix, element [j,i] gives the average
+    deviation of item i with respect to item j.  Note that dev[i,j] =
+    -dev[j,i].
+    For 'counts', element [i,j] gives the number of users who have
+    rated both item i and item j.
+
+    Parameters:
+    M -- NumPy array for utility matrix (each row is a user, each column
+         an item, and each element a rating)
+    mask -- Boolean array (same shape as M) which is True for elements of
+            :M: that are observed
+
+    Returns:
+    dev -- NumPy array of shape (n,n), if M.shape = (m,n).
+    counts -- NumPy array of shape (n,n)
+    """
+    m,n = M.shape
+    dev = np.zeros((n,n))
+    counts = np.zeros((n,n))
+    for i in range(n):
+        for j in range(i + 1, n):
+            # mask_ij[u] is True if user u (i.e. row u of M or mask)
+            # contains ratings for both items i and j, otherwise False
+            mask_ij = np.logical_and(mask[:,i], mask[:,j])
+            diffs = M[mask_ij, j] - M[mask_ij, i]
+            denom = mask_ij.sum()
+            s = diffs.sum() / max(1, denom)
+            dev[j, i] = s
+            dev[i, j] = -s
+            counts[i, j] = denom
+            counts[j, i] = denom
+    return dev, counts
+
+def predict(M, mask, dev, counts, u, j, weighted = False):
+    """Predicts, based on the "Slope One" scheme, how user :u: would rate
+    item :j:.
+
+    Parameters:
+    M -- NumPy array with utility matrix (see :deviation: function)
+    mask -- Boolean array giving observed elements (see :deviation:)
+    dev -- Deviation matrix computed over same set of items
+    counts -- Matrix of counts for each pair; counts[i,j] is the number of
+              users who have rated both item i and item j
+    u -- User index (i.e. row in :M:)
+    j -- Item index (i.e. column in :M:)
+    weighted -- If True, use Weighted Slope One rather than just Slope One.
+                (default False)
+
+    Returns:
+    p -- Predicted rating (as a float) for user u and item j
+    """
+    m,n = M.shape
+    # S_u is a mask over M's columns for items user 'u' rated:
+    S_u = mask[u, :]
+    if weighted:
+        # In 'Weighted Slope One', we sum over everything user 'u' rated,
+        # regardless of whether other users rated both this and item j:
+        S_u[j] = False
+        c_j = counts[j, S_u]
+        devs = dev[j, S_u]
+        u = M[u, S_u]
+        return ((devs + u) * c_j).sum() / max(1.0, c_j.sum())
+    else:
+        # In the 'Slope One' formula we are summing over R_j, which is:
+        # Every item 'i' (i != j), such that: user 'u' rated item 'i', and
+        # at least one other user rated both item 'i' and item 'j'.
+        # Below we compute this likewise as a mask over M's columns:
+        R_j = S_u * (counts[u, :] > 0)
+        R_j[j] = False
+        u = M[u, R_j].sum()
+        devs = dev[j, R_j].sum()
+        card = max(1.0, R_j.sum())
+        return (u + devs) / card
